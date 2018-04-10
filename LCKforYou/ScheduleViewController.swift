0b9872a9017d6a0e1d208719b6820ba8e2bde857
Seungyeon Lee <seungyeonlee@adie-macbook-pro.local>
2018-04-11 00:30:21 +0900

    각 날짜에 맞는 경기이름과 시간 보여주도록 수정

--- LCKforYou/ScheduleViewController.swift
+++ LCKforYou/ScheduleViewController.swift
@@ -34,25 +34,18 @@ class ScheduleViewController: UIViewController, UITableViewDelegate, UITableView
         
         // Realm 저장 위치 보여줌
         print(Realm.Configuration.defaultConfiguration.fileURL!)
-        
-//        items = realm.objects(schedule.self)
         items = realm.objects(match.self)
-
-//        dateFormatter.dateFormat = "MM월 dd일"
     
         //중복되는 날이 없도록 함. 흠 근데 이거 쓰려면 새해마다 데이터 다 삭제해야겠네
-//        for i in items! {
-//            if matchup_dates.contains(i.mmdd_date) {
-//
-//            } else {
-//                matchup_dates.append(i.mmdd_date)
-//            }
-//        }
-        //날짜 순으로 정렬.
-//        matchup_dates.sort()
-        //test
-//        print("04월 08일의 id:\(list["04월 08일"])")
-//        print("key:\(list.values)")
+        for i in items! {
+            if matchup_dates.contains(i.mmdd_date) {
+
+            } else {
+                matchup_dates.append(i.mmdd_date)
+            }
+        }
+//        날짜 순으로 정렬.
+        matchup_dates.sort()
 
     }
     
@@ -62,47 +55,22 @@ class ScheduleViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
 
-    // 각 섹션 당 셀수 얻기 (tableView 구현 필수)
+    // 각 섹션 당 셀 수 얻기
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return (items?.count)!
+        return (items?.filter("mmdd_date ==%@",matchup_dates[section]).count)!
     }
     
-    // 셀 내용 변경하기 (tableView 구현 필수)
+    // 셀 내용
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "List Cell") as! TableViewCell
 //        cell.cellView.layer.cornerRadius = 10
+        //date to string
+        dateFormatter.dateFormat = "hh시"
         
-
-        //realm
-//        let item = items![indexPath.row]
-        //현재 섹션의 튜플 가져오기
-//        let sectiontitle = items![indexPath.section]
-        
-        //titleLabel
-        cell.TitleLabel.text = "ggod"
-//        cell.TitleLabel.text = items?.filter("matchup_date == %@",matchup_dates[indexPath.row])
-//        cell.TitleLabel.text = items?.filter("matchdate <= %@ and >%@",[])
-        //dateLabel
-////        date to string
-//        let dateFormatter = DateFormatter()
-//        dateFormatter.dateFormat = "yyyy-MM-dd"
-//        let dateString = dateFormatter.string(from:matchup_dates[indexPath.section] as Date)
-//        let starttmp = dateString+" 00:00:00"
-//        let endtmp = dateString+" 12:59:59"
-//        print("tmp시작날:\(starttmp)")
-//        print("tmp끝나는날:\(endtmp)")
-//        //string to date
-////        var dateFormatter = DateFormatter()
-//        dateFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
-//
-//        let start = dateFormatter.date(from: starttmp)
-//        let end = dateFormatter.date(from: endtmp)
-//        print("시작일:\(String(describing: start))")
-//        print("끝나는날:\(String(describing: end))")
-////        cell.TitleLabel.text = items?.filter("date contains[c] %@",dateString).value(forKey: "matchtitle") as! String
-////        print("날짜당\(item.matchdate)")
-////        print("스트링\(dateString)")
-//        cell.TitleLabel.text = items?.filter("matchdate >= %@",matchup_dates[indexPath.section]) [indexPath.row].matchtitle
+        let data = items?.filter("mmdd_date == %@",matchup_dates[indexPath.section])[indexPath.row]
+        let startTime = dateFormatter.string(from:(data?.matchdate)!)
+        cell.TitleLabel.text = data?.matchtitle
+        cell.dateLabel.text = startTime
         
 
         return cell
