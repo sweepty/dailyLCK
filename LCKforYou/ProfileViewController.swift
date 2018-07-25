b48af60e83d66cdfb82eb9bbc1abf856e298c662
Seungyeon Lee <adie0423@gmail.com>
2018-07-26 06:03:47 +0900

    [삭제] 구글 로그인 보류 - 에러 때문에 잠시 보류하기로 결정

--- LCKforYou/ProfileViewController.swift
+++ LCKforYou/ProfileViewController.swift
@@ -9,58 +9,13 @@
 import UIKit
 import Firebase
 import FirebaseAuth
-import GoogleSignIn
 
-class ProfileViewController: UIViewController, GIDSignInUIDelegate {
+class ProfileViewController: UIViewController{
     
-
-    
-    @IBOutlet weak var googleSigninButton: GIDSignInButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        GIDSignIn.sharedInstance().uiDelegate = self
-        GIDSignIn.sharedInstance().signIn()
-        
-        // TODO(developer) Configure the sign-in button look/feel
-        // ...
-        
-    }
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        // ...
-        if let error = error {
-            // ...
-            return
-        }
-        
-        guard let authentication = user.authentication else { return }
-        let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
-                                                       accessToken: authentication.accessToken)
-        // ...
-        Auth.auth().signIn(with: credential) { (user, error) in
-            if let error = error {
-                // ...
-                return
-            }
-            // User is signed in
-            // ...
-        }
         
     }
     
-    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
-        // Perform any operations when the user disconnects from app here.
-        // ...
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
