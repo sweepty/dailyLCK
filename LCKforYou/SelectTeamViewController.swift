e60702bd87bda08e63566ab15d5002db30f0f231
Seungyeon Lee <adie0423@gmail.com>
2018-08-01 03:50:30 +0900

    [수정] 팀 선택 뷰 컬렉션뷰로 변경 - 테이블뷰에서 컬렉션뷰로 변경

--- LCKforYou/SelectTeamViewController.swift
+++ LCKforYou/SelectTeamViewController.swift
@@ -8,32 +8,41 @@
 
 import UIKit
 
-class SelectTeamViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+class SelectTeamViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet var collectionView: UICollectionView!
     
     var teamList = ["Afreeca Freecs", "bbq Olivers", "Gen.G", "Griffin", "Hanwha Life Esports", "Jin Air Greenwings", "KING-ZONE DragonX" ,"KT Rolster", "MVP" ,"SK Telecom T1"]
     
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return teamList.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "List Cell") as! TeamCell
-//        cell.textLabel?.text = teamList[indexPath.row]
-        cell.logo.image = UIImage(named: "hie.png")
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "teamCollectionViewCell", for: indexPath) as! teamCollectionViewCell
+        
+//        switch teamList[indexPath.row] {
+//        case 0: cell.logo.image = UIImage(named: "afs.png")
+//        case 1: cell.logo.image = UIImage(named: "bbq.png")
+//        case 2: cell.logo.image = UIImage(named: "ksv.png")
+//        case 3: cell.logo.image = UIImage(named: "griffin.png")
+//        case 4: cell.logo.image = UIImage(named: "hie.png")
+//        case 5: cell.logo.image = UIImage(named: "jag.png")
+//        case 6: cell.logo.image = UIImage(named: "kz.png")
+//        case 7: cell.logo.image = UIImage(named: "kt.png")
+//        case 8: cell.logo.image = UIImage(named: "mvp.png")
+//        default:
+//            cell.logo.image = UIImage(named: "skt.png")
+//        }
+        //로고 임시용
+        cell.logo.image = UIImage(named: "skt.png")
         cell.name.text = teamList[indexPath.row]
+        
         return cell
     }
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.delegate = self
-        tableView.dataSource = self
-        
         teamList.sort()
     }
 }
---
+++ LCKforYou/teamCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  teamCollectionViewCell.swift
+//  LCKforYou
+//
+//  Created by Seungyeon Lee on 01/08/2018.
+//  Copyright © 2018 Seungyeon Lee. All rights reserved.
+//
+
+import UIKit
+
+class teamCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var logo: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
