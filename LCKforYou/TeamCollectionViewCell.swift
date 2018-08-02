e4215f01541f596965aac3e46cf20fc46f674cb1
Seungyeon Lee <adie0423@gmail.com>
2018-08-03 07:52:15 +0900

    [수정] 버튼으로 변경 - 이미지에서 버튼으로 변경. - 파일명 변경으로 인해 삭제 후 다시 생성함.

---
+++ LCKforYou/TeamCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TeamCollectionViewCell.swift
+//  LCKforYou
+//
+//  Created by Seungyeon Lee on 03/08/2018.
+//  Copyright © 2018 Seungyeon Lee. All rights reserved.
+//
+
+import UIKit
+
+class TeamCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var button: UIButton!
+}
