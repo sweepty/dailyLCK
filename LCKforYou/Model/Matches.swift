c7c518079568236b9d3aae040cd571b7297c7769
Seungyeon Lee <adie0423@gmail.com>
2018-09-04 02:25:57 +0900

    [추가] codable 채택한 matches struct 생성 - json 파싱하기 위함

---
+++ LCKforYou/Model/Matches.swift
@@ -0,0 +1,29 @@
+//
+//  MatchCodableModel.swift
+//  LCKforYou
+//
+//  Created by Seungyeon Lee on 03/09/2018.
+//  Copyright © 2018 Seungyeon Lee. All rights reserved.
+//
+
+import Foundation
+
+struct Matches: Codable {
+    var matchId: Int
+    var matchDate: String
+    var ticketDate: String
+    var leftTeam: String
+    var rightTeam: String
+    var stadium: String
+    var season: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case matchId = "match_id"
+        case matchDate = "match_date"
+        case ticketDate = "ticket_date"
+        case leftTeam = "left_team"
+        case rightTeam = "right_team"
+        case stadium = "stadium"
+        case season = "season_name"
+    }
+}
