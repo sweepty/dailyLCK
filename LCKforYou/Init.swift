97a3760cb8bf7f08544ac490fec2b0406c9f35dd
Seungyeon Lee <adie0423@gmail.com>
2018-09-10 01:15:50 +0900

    [추가] realm에 경기 일정 저장 - 서버에서 경기 일정을 받아 realm에 저장하도록 함

--- LCKforYou/Init.swift
+++ LCKforYou/Init.swift
@@ -6,25 +6,70 @@
 //  Copyright © 2018년 Seungyeon Lee. All rights reserved.
 //
 
+import UIKit
 import Foundation
 import RealmSwift
 
 class Init {
     var realm: Realm!
+    let dateFormatter = DateFormatter()
+    var notificationToken: NotificationToken!
     
     init() {
         realm = try! Realm()
         
-        Team.add(1, "Afreeca Freecs", false)
+        dataTask(url: "http://127.0.0.1:3000/matches", method: "GET")
+
+        Team.add(1, "Afreeca", false)
         Team.add(2, "Gen.G", false)
         Team.add(3, "Griffin", false)
-        Team.add(4, "Hanwha Life Esports", false)
-        Team.add(5, "Jin Air Greenwings", false)
-        Team.add(6, "KING-ZONE DragonX", false)
-        Team.add(7, "KT Rolster", false)
+        Team.add(4, "Hanwha", false)
+        Team.add(5, "Jin Air", false)
+        Team.add(6, "KING-ZONE", false)
+        Team.add(7, "KT", false)
         Team.add(8, "MVP", false)
-        Team.add(9, "SK Telecom T1", false)
-        Team.add(10, "bbq Olivers", false)
+        Team.add(9, "SKT", false)
+        Team.add(10, "bbq", false)
+
+    }
+    
+    func dataTask(url: String, method: String) {
+        var request = URLRequest(url: URL(string: url)!)
+        request.httpMethod = method
+        
+        let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
+            let decoder = JSONDecoder()
+            self.dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+            decoder.dateDecodingStrategy = .formatted(self.dateFormatter)
+
+            if let data = data, let matchList = try? decoder.decode([Matches].self, from: data) {
+
+                DispatchQueue(label: "background").async {
+                    autoreleasepool {
+                        let realm = try! Realm()
+                        
+                        for m in matchList {
+                            realm.beginWrite()
+                            
+                            let matchItem = Match()
+                            matchItem.id = m.id
+                            matchItem.date = m.date
+                            matchItem.ticketDate = m.ticketDate
+                            matchItem.teamLeft = m.teamLeft
+                            matchItem.teamRight = m.teamRight
+                            matchItem.stadium = m.stadium
+                            matchItem.season = m.season
+                            
+                            realm.create(Match.self, value: matchItem)
+                            try! realm.commitWrite()
+                        }
+                    }
+                }
+
+            }
+        }
+        task.resume()
         
     }
 }
+
