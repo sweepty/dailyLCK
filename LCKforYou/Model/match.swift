7e4bbeb55c5edf921b10ddad8d2a962f0820c7b9
Seungyeon Lee <adie0423@gmail.com>
2018-09-09 21:10:03 +0900

    [수정] realm match attribute 수정 - title, round, mmdd_date 삭제

--- LCKforYou/Model/match.swift
+++ LCKforYou/Model/match.swift
@@ -12,15 +12,12 @@ import RealmSwift
 
 class Match: Object {
     @objc dynamic var id = 0
-    @objc dynamic var title: String = ""
     @objc dynamic var date = Date()
     @objc dynamic var ticketDate = Date()
     @objc dynamic var teamLeft: String = ""
     @objc dynamic var teamRight: String = ""
     @objc dynamic var stadium: String = "" // 상암,강남, 그 외...
     @objc dynamic var season: String = "" // spring(LCK), summer(LCK), lift rivals, msi, world championship, kespa cup(LCK + 지역)
-    @objc dynamic var round: String = "" // round 1, round2, post season, final
-    @objc dynamic var mmdd_date: String = "" // section name용 ex) "4월 23일"
     
     //primary key 설정
     override static func primaryKey() -> String? {
