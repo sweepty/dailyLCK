b2d5c069b4874449e65ec206fd8ebd11a23b1ed7
Seungyeon Lee <adie0423@gmail.com>
2018-09-10 01:15:17 +0900

    [수정] codingkey로 변경

--- LCKforYou/Model/Matches.swift
+++ LCKforYou/Model/Matches.swift
@@ -9,21 +9,21 @@
 import Foundation
 
 struct Matches: Codable {
-    var matchId: Int
-    var matchDate: Date
+    var id: Int
+    var date: Date
     var ticketDate: Date
-    var leftTeam: String
-    var rightTeam: String
+    var teamLeft: String
+    var teamRight: String
     var stadium: String
-    var seasonName: String
+    var season: String
     
-//    private enum CodingKeys: String, CodingKey {
-//        case matchId = "match_id"
-//        case matchDate = "match_date"
-//        case ticketDate = "ticket_date"
-//        case leftTeam = "left_team"
-//        case rightTeam = "right_team"
-//        case stadium = "stadium"
-//        case season = "season_name"
-//    }
+    private enum CodingKeys: String, CodingKey {
+        case id = "match_id"
+        case date = "match_date"
+        case ticketDate = "ticket_date"
+        case teamLeft = "left_team"
+        case teamRight = "right_team"
+        case stadium
+        case season = "season_name"
+    }
 }
