73ea4173d9a64c014bb7861715fee3c73c0d6a29
Seungyeon Lee <adie0423@gmail.com>
2018-09-19 23:59:56 +0900

    [추가] local notification authorization

--- LCKforYou/AppDelegate.swift
+++ LCKforYou/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import UserNotifications
 
 var uiRealm  = try! Realm()
 
@@ -15,8 +16,20 @@ var uiRealm  = try! Realm()
 class AppDelegate: UIResponder, UIApplicationDelegate{
 
     var window: UIWindow?
+    let center = UNUserNotificationCenter.current()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+//        UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+        let options: UNAuthorizationOptions = [.alert, .sound]
+        // Swift
+        center.requestAuthorization(options: options) {
+            (granted, error) in
+            if !granted {
+                print("Something went wrong")
+            }
+        }
+        
+        
         let launchedBefore = UserDefaults.standard.bool(forKey: "launchedBefore")
         if launchedBefore  {
             print("Not first launch.")
