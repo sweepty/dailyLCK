6d7e9e7f91d080f804875f8e475fdd0a991b303c
Seungyeon Lee <adie0423@gmail.com>
2019-02-04 05:48:58 +0900

    [수정] request 코드 변경 - 파일명 변경 - alamofire 사용 - 받아온 데이터를 배열에 넣도록 함

---
+++ LCKforYou/Network/API.swift
@@ -0,0 +1,14 @@
+//
+//  API.swift
+//  LCKforYou
+//
+//  Created by Seungyeon Lee on 01/02/2019.
+//  Copyright © 2019 Seungyeon Lee. All rights reserved.
+//
+
+import Foundation
+
+class API {
+    public static let baseURL = "http://127.0.0.1:3000/"
+    
+}
--- LCKforYou/Network/Requests.swift
+++ LCKforYou/Network/Requests.swift
@@ -1,67 +1,41 @@
 //
-//  Init.swift
+//  Requests.swift
 //  LCKforYou
 //
-//  Created by Seungyeon Lee on 2018. 8. 8..
-//  Copyright © 2018년 Seungyeon Lee. All rights reserved.
+//  Created by Seungyeon Lee on 04/02/2019.
+//  Copyright © 2019 Seungyeon Lee. All rights reserved.
 //
 
-import UIKit
 import Foundation
+import UIKit
+import Realm
 import RealmSwift
+import Alamofire
 
-class Init {
+class Requests {
     var realm: Realm!
     let dateFormatter = DateFormatter()
     var notificationToken: NotificationToken!
-    
-    init() {
-        realm = try! Realm()
-        dataTask(url: "http://127.0.0.1:3000/matches", method: "GET")
-        insertTeams()
-    }
-    
-    func dataTask(url: String, method: String) {
-        var request = URLRequest(url: URL(string: url)!)
-        request.httpMethod = method
-        
-        let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
+
+    func getMatchInfo(completion: @escaping (Bool, [Matches]) -> Void) {
+        Alamofire.request(API.baseURL).responseJSON { response in
+            print("Result: \(response.result)")
             let decoder = JSONDecoder()
             self.dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
             decoder.dateDecodingStrategy = .formatted(self.dateFormatter)
             decoder.keyDecodingStrategy = .convertFromSnakeCase
             
-            if let data = data, let matchList = try? decoder.decode([Matches].self, from: data) {
+            if let data = response.data, let matches = try? decoder.decode([Matches].self, from: data) {
+//                print("Data: \(matches)")
+                CalendarViewController.matchList = matches
                 
                 
-                DispatchQueue(label: "background").async {
-                    autoreleasepool {
-                        let realm = try! Realm()
-                        realm.beginWrite()
-                        for m in matchList {
-                            
-                            
-                            let matchItem = Match()
-                            matchItem.id = m.matchId
-                            matchItem.date = m.matchDate
-                            matchItem.ticketDate = m.ticketDate
-                            matchItem.teamLeft = m.leftTeam
-                            matchItem.teamRight = m.rightTeam
-                            matchItem.stadium = m.stadium
-                            matchItem.season = m.seasonName
-                            
-                            realm.create(Match.self, value: matchItem)  
-                        }
-                        try! realm.commitWrite()
-                    }
-                    
-                    
+                DispatchQueue.main.async {
+                    completion(true, CalendarViewController.matchList)
                 }
-                
+                print("변경완료")
             }
         }
-        task.resume()
-//        ScheduleViewController().tableView?.reloadData()
     }
     
     func insertTeams() {
@@ -85,7 +59,7 @@ class Init {
                             teamItem.id = t.teamId
                             teamItem.name = t.teamName
                             teamItem.heart = false
-
+                            
                             
                             realm.create(Team.self, value: teamItem)
                             try! realm.commitWrite()
